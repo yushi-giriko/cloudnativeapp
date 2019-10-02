88765c320965d571abba041829788df1218cf585
gustavogaspar <gaspar.gualves@gmail.com>
2019-10-02 19:16:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,18 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
 var request = require("request");
 
-app.use(express.static(__dirname + '/src'));
-
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + 'index.html');
-});
-
-io.on('connection', function (socket) {
-    socket.on('random', function (id) {
-        console.log('randomized id: ' + id);
-
-        var options = {
-            method: 'GET',
-            url: `${process.env.DBURI}?id=${id}`,
-            headers:
-            {
-                'Postman-Token': 'c55ef931-4167-420e-9ae8-0d62c5be2588',
-                'cache-control': 'no-cache'
-            }
-        };
-        request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-            console.log(body);
-            io.emit('randomprofile', body)
-        });
-
-    });
+    var options = {
+        method: 'GET',
+        url: `${process.env.DBURI}?id=${Math.floor(Math.random()*1000)}`,
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+        res.send(body);
+    });    
 });
 
 http.listen(80, function () {
