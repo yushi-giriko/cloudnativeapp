68de78c4bc24907a062db1b57e6866bde633c344
gustavogaspar <gaspar.gualves@gmail.com>
2019-10-17 16:37:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var request = require("request");
 app.get('/', function (req, res) {
     var options = {
         method: 'GET',
-        url: `<YOUR ORDS ENDPOINT>?id=${Math.floor(Math.random()*1000)}`,
+        url: `https://vqsotyjm0kap0h0-workshopatp.adb.us-ashburn-1.oraclecloudapps.com/ords/ords/api/users/`,
     };
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
